3718d72b920439fb001d9f10ba4eb69d054a4608
gutalks <ghenriquesantosferreira@gmail.com>
2026-09-06 19:54:48 -0300

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,1 +1,9 @@
+const SUPABASE_URL = "https://ddwzuqtnkkdacturmndu.supabase.co";
 
+const SUPABASE_KEY = "COLE_AQUI_SUA_PUBLISHABLE_KEY";
+
+// Cria a conexão com o Supabase
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
